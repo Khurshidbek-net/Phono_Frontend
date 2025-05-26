cc05785ad9de6e6a635ed666ec54c05921c39572
Khurshidbek <xurshidbekoktamaliyev651@gmail.com>
2025-05-26 15:22:30 +0500

    first commit

---
+++ next.config.ts
@@ -0,0 +1,12 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  reactStrictMode: true,
+  devIndicators: false,
+  compiler: {
+    styledComponents: true,
+  }
+};
+
+export default nextConfig;
---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+import { HomePage } from "../app";
+
+export default function Home() {
+  return (
+    <div>
+      <main>
+        <HomePage/>
+      </main>
+    </div>
+  );
+}
